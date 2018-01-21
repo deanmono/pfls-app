9395d22edc40cc9461890e6d5c1eaf21020fbcf9
Dean M <deanmono@gmail.com>
2018-01-21 02:18:58 -0500

    removed arb component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-import { ArbitrageComponent } from "./arbitrage/arbitrage.component";
 
 const routes: Routes = [
-    { path: 'arbitrage', component: ArbitrageComponent },
     { path: '', component: HomeComponent }
 ];
 
